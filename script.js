b480a81967d7d310a111dc9ddf5d03393b51607b
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-03 20:44:24 +0100

    Added roving marker and pop-up base from Leaflet

--- script.js
+++ script.js
@@ -14,18 +14,31 @@ if(navigator.geolocation) {
 navigator.geolocation.getCurrentPosition(function(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.pt.maps/@${latitude},${longitude}`);
 
-    const map = L.map(`map`).setView([latitude, longitude], 13);
+    const coords = [latitude, longitude];
+
+    const map = L.map(`map`).setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    })
+        .addTo(map);
 
-    L.marker([latitude, longitude]).addTo(map)
-    .bindPopup('Your current location')
-    .openPopup();
-
+    map.on(`click`, function(mapEvent) {
+        console.log(mapEvent);
+        const newCoords = mapEvent.latlng;
+        L.marker(newCoords)
+            .addTo(map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: `running-popup`,
+            }))
+            .setPopupContent(`Chosen location`)
+            .openPopup();
+    });
 
     }, function() {
         alert(`Could not retrieve position`);
