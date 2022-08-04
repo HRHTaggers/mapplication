640709604464ca5be53b51a862c5ce92ed06410b
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-04 10:49:18 +0100

    Created Cycling/Running classes

--- script.js
+++ script.js
@@ -11,6 +11,48 @@ const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
 
+class Workout {
+    date = new Date();
+    id = (Date.now() + ``).slice(-10);
+
+    constructor(coords, distance, duration) {
+        this.coords = coords; //lat, lng
+        this.distance = distance; // km
+        this.duration = duration; // mins
+    }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence; //steps
+    this.calcPace();
+  }
+
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevation) {
+    super(coords, distance, duration);
+    this.elevation = elevation; // ms
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+
+//////////////////////////////////////
+//APPLICATION ARCHITECTURE
 class App {
     #map;
     #mapEvent;
