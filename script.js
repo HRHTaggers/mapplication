45ecdd495dedf29a6b3e70661fa4139233539979
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-08-04 21:41:50 +0100

    Added interactive marker focus and clicks functions

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 class Workout {
     date = new Date();
     id = (Date.now() + ``).slice(-10);
-
+    clicks = 0;
 
     constructor(coords, distance, duration) {
         this.coords = coords; //lat, lng
@@ -29,6 +29,10 @@ class Workout {
     ];
 
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click() {
+        this.clicks++;
     }
 }
 
@@ -80,6 +84,7 @@ const inputElevation = document.querySelector(".form__input--elevation");
 
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -89,6 +94,8 @@ class App {
         form.addEventListener(`submit`, this._newWorkout.bind(this));
 
         inputType.addEventListener(`change`, this._toggleElevationField);
+    
+        containerWorkouts.addEventListener(`click`, this._moveToPopup.bind(this));
     }
 
     _getPosition() {
@@ -106,7 +113,7 @@ class App {
 
         const coords = [latitude, longitude];
 
-        this.#map = L.map(`map`).setView(coords, 13);
+        this.#map = L.map(`map`).setView(coords, this.#mapZoomLevel);
 
         L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -261,6 +268,24 @@ class App {
 
         form.insertAdjacentHTML(`afterend`, html);
     }
+    
+    _moveToPopup(event) {
+        const workoutElement = event.target.closest(`.workout`);
+
+        if(!workoutElement) return;
+
+        const workout = this.#workouts.find(work => work.id === workoutElement.dataset.id);
+        
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+        animate: true,
+        pan: {
+            duration: 1,
+        }
+    });
+
+    workout.click();
+    }
+
 }
 
 const app = new App();
